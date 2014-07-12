0d82b12e6cb20ad584fa47b2b9c2f55dd7b9926e
Bill Piel <bill@billpiel.com>
2014-07-12 10:33:35 -0400

    process-grammar working and tested

--- src/wutchamean/core.clj
+++ src/wutchamean/core.clj
@@ -2,28 +2,27 @@
   (:require [clojure.string :refer [split lower-case]]))
 
 
+;(group-by first (split-words-in-string (second rule)))
+
 (defn process-grammar
   [grammar]
-  (map (fn [rule]  
-           (map         
-             (fn [word-pair]
-                 (vector
-                   (first word-pair)
-                   (first rule)
-                   (second word-pair)
-                   ))
-             (flatten (map #(vector (split % #" ") %) (second rule)))))
-       (seq grammar)))
+  (group-by first
+            (apply concat (map (fn [rule]
+                                 (map (fn [word-pair]
+                                        (assoc-in word-pair [1 :class] (first rule)))
+                                      (split-words-in-string-list (second rule))))
+                               (seq grammar)))))
 
 
 
-(defn split-words-in-string
+(defn split-words-in-string-list
   [word-list]
-  (apply concat (map (fn [tuple] 
-                         (map (fn [word] 
-                                  [(lower-case word) {:string (second tuple) :index (nth tuple 2)}])
-                              (first tuple)))
-                      (map-indexed #(vector (split %2 #"[^\w\d']+") %2 %)
+  (apply concat
+         (map (fn [tuple] 
+                (map (fn [word] 
+                       [(lower-case word) {:string (second tuple) :index (nth tuple 2)}])
+                     (first tuple)))
+              (map-indexed #(vector (split %2 #"[^\w\d']+") %2 %)
                            word-list))))
 
 (defn match-tokens
--- test/wutchamean/core_test.clj
+++ test/wutchamean/core_test.clj
@@ -4,23 +4,56 @@
 
 (def grammar
   {:grammar {
-    :animal ["dog" "cat" "hunted wumpus" "superb lyrebird of southern australia" "Australian Bear" "Austrian Cat Southern-style"]
-    :person ["Bill" "Kris" "Trevor" "Cat Woman" "Dogwoman" "Bird Man" "hunter" "Australian" "Austrian" "Warren Beatty"]}})
+             :animal ["dog" "cat" "hunted wumpus" "superb lyrebird of southern australia" "Australian Bear" "Austrian Cat Southern-style"]
+             :person ["Bill" "Kris" "Trevor" "Cat Woman" "Dogwoman" "Bird Man" "hunter" "Australian" "Austrian" "Warren Beatty"]}})
 
-(fact "strings get split"
-  (split-words-in-string (-> grammar :grammar :animal)) 
-  => [["dog" {:index 0, :string "dog"}]
-	  ["cat" {:index 1, :string "cat"}]
-	  ["hunted" {:index 2, :string "hunted wumpus"}]
-	  ["wumpus" {:index 2, :string "hunted wumpus"}]
-	  ["superb" {:index 3, :string "superb lyrebird of southern australia"}]
-	  ["lyrebird" {:index 3, :string "superb lyrebird of southern australia"}]
-	  ["of" {:index 3, :string "superb lyrebird of southern australia"}]
-	  ["southern" {:index 3, :string "superb lyrebird of southern australia"}]
-	  ["australia" {:index 3, :string "superb lyrebird of southern australia"}]
-	  ["australian" {:index 4, :string "Australian Bear"}]
-	  ["bear" {:index 4, :string "Australian Bear"}]
-	  ["austrian" {:index 5, :string "Austrian Cat Southern-style"}]
-	  ["cat" {:index 5, :string "Austrian Cat Southern-style"}]
-	  ["southern" {:index 5, :string "Austrian Cat Southern-style"}]
-	  ["style" {:index 5, :string "Austrian Cat Southern-style"}]])
+#_(fact "strings get split"
+        (split-words-in-string-list (-> grammar :grammar :animal)) 
+        => [["dog" {:index 0, :string "dog"}]
+            ["cat" {:index 1, :string "cat"}]
+            ["hunted" {:index 2, :string "hunted wumpus"}]
+            ["wumpus" {:index 2, :string "hunted wumpus"}]
+            ["superb" {:index 3, :string "superb lyrebird of southern australia"}]
+            ["lyrebird" {:index 3, :string "superb lyrebird of southern australia"}]
+            ["of" {:index 3, :string "superb lyrebird of southern australia"}]
+            ["southern" {:index 3, :string "superb lyrebird of southern australia"}]
+            ["australia" {:index 3, :string "superb lyrebird of southern australia"}]
+            ["australian" {:index 4, :string "Australian Bear"}]
+            ["bear" {:index 4, :string "Australian Bear"}]
+            ["austrian" {:index 5, :string "Austrian Cat Southern-style"}]
+            ["cat" {:index 5, :string "Austrian Cat Southern-style"}]
+            ["southern" {:index 5, :string "Austrian Cat Southern-style"}]
+            ["style" {:index 5, :string "Austrian Cat Southern-style"}]])
+
+(fact "process-grammar"
+      (process-grammar (:grammar grammar))
+      => {"australia" [["australia" {:class :animal, :index 3, :string "superb lyrebird of southern australia"}]],
+          "australian" [["australian" {:class :person, :index 7, :string "Australian"}] 
+                        ["australian" {:class :animal, :index 4, :string "Australian Bear"}]],
+          "austrian" [["austrian" {:class :person, :index 8, :string "Austrian"}] 
+                      ["austrian" {:class :animal, :index 5, :string "Austrian Cat Southern-style"}]],
+          "bear" [["bear" {:class :animal, :index 4, :string "Australian Bear"}]],
+          "beatty" [["beatty" {:class :person, :index 9, :string "Warren Beatty"}]],
+          "bill" [["bill" {:class :person, :index 0, :string "Bill"}]],
+          "bird" [["bird" {:class :person, :index 5, :string "Bird Man"}]],
+          "cat" [["cat" {:class :person, :index 3, :string "Cat Woman"}] 
+                 ["cat" {:class :animal, :index 1, :string "cat"}] 
+                 ["cat" {:class :animal, :index 5, :string "Austrian Cat Southern-style"}]],
+          "dog" [["dog" {:class :animal, :index 0, :string "dog"}]],
+          "dogwoman" [["dogwoman" {:class :person, :index 4, :string "Dogwoman"}]],
+          "hunted" [["hunted" {:class :animal, :index 2, :string "hunted wumpus"}]],
+          "hunter" [["hunter" {:class :person, :index 6, :string "hunter"}]],
+          "kris" [["kris" {:class :person, :index 1, :string "Kris"}]],
+          "lyrebird" [["lyrebird" {:class :animal, :index 3, :string "superb lyrebird of southern australia"}]],
+          "man" [["man" {:class :person, :index 5, :string "Bird Man"}]],
+          "of" [["of" {:class :animal, :index 3, :string "superb lyrebird of southern australia"}]],
+          "southern" [["southern" {:class :animal, :index 3, :string "superb lyrebird of southern australia"}] 
+                      ["southern" {:class :animal, :index 5, :string "Austrian Cat Southern-style"}]],
+          "style" [["style" {:class :animal, :index 5, :string "Austrian Cat Southern-style"}]],
+          "superb" [["superb" {:class :animal, :index 3, :string "superb lyrebird of southern australia"}]],
+          "trevor" [["trevor" {:class :person, :index 2, :string "Trevor"}]],
+          "warren" [["warren" {:class :person, :index 9, :string "Warren Beatty"}]],
+          "woman" [["woman" {:class :person, :index 3, :string "Cat Woman"}]],
+          "wumpus" [["wumpus" {:class :animal, :index 2, :string "hunted wumpus"}]]}
+      
+      )
