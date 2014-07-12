ceafda5d851d4da1995847a16bcd67d95af06cdd
Bill Piel <bill@billpiel.com>
2014-07-12 09:29:29 -0400

    setup project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,9 @@
 (defproject wutchamean "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+  :description "A fuzzy parser for short natural-language queries with narrow vocabulary"
+  :url ""
+  :license {:name "MIT License"
+            :url "http://www.opensource.org/licenses/mit-license.php"}
+  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :profiles {:uberjar {:aot :all}
+             :dev {:dependencies [[midje "1.6.3"]]
+                   :plugins [[lein-midje "3.1.3"]]}})
